b2832861eec4e4c2f4bf594bc70eace6ee8147e0
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2022-12-12 10:51:03 +0100

    changed template theme to light theme, enabled smooth scroll, connected sections to navigation links

--- index.js
+++ index.js
@@ -2,6 +2,31 @@ const menuBtn = document.getElementById("menu")
 const closeBtn = document.querySelector(".close")
 const navBar = document.querySelector("nav")
 const logo = document.querySelector("#logo")
+const links = document.querySelectorAll(".modal div a")
+
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault()
+    document.querySelector(".modal").classList.remove("show-modal")
+    const id = e.currentTarget.getAttribute("href").slice(1)
+    const element = document.getElementById(id)
+    console.log(id)
+
+    // calculate the heights
+    const navHeight = navBar.getBoundingClientRect().height
+    console.log(navHeight)
+    const fixedNav = navBar.classList.contains("fixed-navbar")
+    let position = element.offsetTop - navHeight
+    if (!fixedNav) {
+      position = position - navHeight
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+  })
+})
 
 menuBtn.addEventListener("click", () => {
   document.querySelector(".modal").classList.add("show-modal")
@@ -11,7 +36,8 @@ closeBtn.addEventListener("click", () => {
   document.querySelector(".modal").classList.remove("show-modal")
 })
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll", (e) => {
+  e.preventDefault()
   const navHeight = navBar.getBoundingClientRect().height
   const scrollHeight = window.pageYOffset
   if (scrollHeight > navHeight) {
